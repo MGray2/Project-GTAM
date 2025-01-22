488d1a4c771f78342ca285b553c01036108bfa45
MGray2 <mgray24@basecampcodingacademy.org>
2025-01-22 11:38:29 -0600

    Created functional button on main menu

--- app/src/main/java/com/example/gtam/Activity1.kt
+++ app/src/main/java/com/example/gtam/Activity1.kt
@@ -14,27 +14,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gtam.ui.theme.GTAMTheme
-import androidx.compose.ui.platform.LocalContext
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            GTAMTheme {
-                MainMenu()
-            }
-        }
-    }
-}
+class Activity1 : ComponentActivity()
+{
 
-
-@Composable
-fun MainMenu() {
-    val context = LocalContext.current
-
-    Button(onClick = {
-        val intent = Intent(context, Activity1::class.java)
-        context.startActivity(intent)
-    }) { Text("Activity 1") }
 }
