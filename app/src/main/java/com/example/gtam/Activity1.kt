914df93e76fe11fdad667d83fc2ae67b743bd852
MGray2 <mgray24@basecampcodingacademy.org>
2025-01-28 12:19:02 -0600

    Info button functionality complete

--- app/src/main/java/com/example/gtam/Activity1.kt
+++ app/src/main/java/com/example/gtam/Activity1.kt
@@ -6,6 +6,7 @@ import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,6 +19,10 @@ import com.example.gtam.ui.theme.GTAMTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -27,8 +32,10 @@ import androidx.compose.ui.unit.sp
 class Activity1 : ComponentActivity()
 {
     private val component = Components()
-
-
+    private val message1 = "This will be the email that the system uses for messaging."
+    private val message2 = "This will be the phone number that the system uses for texting."
+    private val message3 = "The system will default to this starting message should no additional input be included."
+    private val message4 = "The system will default to this closing message should no additional input be included."
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,17 +44,17 @@ class Activity1 : ComponentActivity()
             GTAMTheme {
                 Column(modifier = Modifier) {
                     component.CustomHeader("Main Account Setup")
-                    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-                        Spacer(modifier = Modifier.weight(1F))
-                        LittleText("Bot Email")
-                        component.InfoButton()
-                    }
+                    // Bot Email
+                    LittleTextWIButton("Bot Email", component, message1)
                     component.InputField("Your Email Here")
-                    LittleText("Bot Phone Number")
+                    // Bot Phone Number
+                    LittleTextWIButton("Bot Phone Number", component, message2)
                     component.InputField("Your Phone Number Here")
-                    LittleText("Message Header")
+                    // Message Header
+                    LittleTextWIButton("Message Header", component, message3)
                     component.InputFieldLarge("Message")
-                    LittleText("Message Footer")
+                    // Message Footer
+                    LittleTextWIButton("Message Footer", component, message4)
                     component.InputFieldLarge("Message")
                 }
 
@@ -57,13 +64,23 @@ class Activity1 : ComponentActivity()
 }
 
 @Composable
-private fun LittleText(text: String) {
+private fun LittleText(text: String, modifier: Modifier) {
     Text(text, fontSize = 22.sp,
         textAlign = TextAlign.Center,
         modifier = Modifier.wrapContentWidth()
             .padding(0.dp, 10.dp, 0.dp, 0.dp)
             )
 
+}
+
+@Composable
+private fun LittleTextWIButton(text: String, component: Components, message: String) {
+    Row(modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center) {
+        LittleText(text, Modifier.weight(1F))
+        component.InfoButton(message)
+    }
 }
 
 
--- app/src/main/java/com/example/gtam/ui/theme/Components.kt
+++ app/src/main/java/com/example/gtam/ui/theme/Components.kt
@@ -4,12 +4,14 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -20,14 +22,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.gtam.R
 
 
@@ -98,9 +103,10 @@ class Components {
     }
 
     @Composable
-    fun InfoButton()
+    fun InfoButton(message: String)
     {
-        Button(onClick = {},
+        var showDialog by remember { mutableStateOf(false) }
+        Button(onClick = { showDialog = true },
             contentPadding = PaddingValues(0.dp),
             modifier = Modifier.size(20.dp),
             colors = ButtonDefaults.buttonColors(
@@ -111,6 +117,36 @@ class Components {
             Icon(painter = painterResource(id = R.drawable.info),
                 contentDescription = "Info",
                 modifier = Modifier.size(20.dp))
+        }
+
+        if (showDialog) {
+            CustomMessageBox(
+                showDialog = showDialog,
+                onDismiss = { showDialog = false },
+                message = message
+            )
+        }
+    }
+
+    @Composable
+    fun CustomMessageBox(showDialog: Boolean, onDismiss: () -> Unit, message: String) {
+        if (showDialog) {
+            Dialog(onDismissRequest = { onDismiss() }) {
+                Box(
+                    modifier = Modifier
+                        .size(300.dp, 200.dp)
+                        .background(Color.White, shape = RoundedCornerShape(16.dp))
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = message,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+            }
         }
     }
 
